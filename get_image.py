3fa670e9883a1e447c6abb01f3fb09d8d4afd469
Massimo Bottelli <88494940+massimobottelli@users.noreply.github.com>
2024-11-14 19:01:37 +0100

    Add files via upload

--- get_image.py
+++ get_image.py
@@ -1,32 +1,40 @@
 from picamera2 import Picamera2
 from datetime import datetime
+from PIL import Image
 import os
 import paramiko
 
 # Configura le credenziali e i dettagli del server
-server_address = "indirizzo_IP_del_server"
-username = "nome_utente"
-password = "password"
-remote_directory = "/path/remoto/dove/salvare"
+server_address = "192.168.1.143"
+username = "massimo"
+password = "maxmil"
+remote_path = "/Users/massimo/image.jpg"
+local_path = "/home/massimo/thermovision/image.jpg"  # Percorso e nome dell'immagine ridimensionata
 
 
-def capture_image_with_timestamp():
+def capture_and_resize_image():
     # Crea un'istanza di Picamera2
     picam2 = Picamera2()
 
-    # Configura la fotocamera e acquisisce l'immagine
-    timestamp = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
-    filename = f"{timestamp}.jpg"
-    local_path = os.path.join("/path/locale/dove/salvare", filename)
+    # Nome temporaneo per l'immagine acquisita
+    temp_image_path = "/home/massimo/thermovision/temp_image.jpg"
 
-    # Cattura e salva l'immagine
-    picam2.start_and_capture_file(local_path)
-    print(f"Immagine salvata localmente come {local_path}")
+    # Cattura e salva l'immagine in un file temporaneo
+    picam2.start_and_capture_file(temp_image_path)
+    print(f"Immagine acquisita e salvata temporaneamente come {temp_image_path}")
 
-    return local_path, filename
+    # Ridimensiona l'immagine a 640x480
+    with Image.open(temp_image_path) as img:
+        resized_img = img.resize((640, 480))
+        resized_img.save(local_path)
+        print(f"Immagine ridimensionata e salvata come {local_path}")
+
+    # Rimuovi il file temporaneo
+    os.remove(temp_image_path)
+    return local_path
 
 
-def transfer_file_via_sftp(local_path, remote_directory, filename):
+def transfer_file_via_sftp(local_path, remote_path):
     try:
         # Crea un client SSH e connettiti al server
         ssh = paramiko.SSHClient()
@@ -35,9 +43,6 @@ def transfer_file_via_sftp(local_path, remote_directory, filename):
 
         # Crea un client SFTP dalla connessione SSH
         sftp = ssh.open_sftp()
-
-        # Definisce il percorso di destinazione sul server
-        remote_path = os.path.join(remote_directory, filename)
 
         # Trasferisce il file
         sftp.put(local_path, remote_path)
@@ -52,6 +57,6 @@ def transfer_file_via_sftp(local_path, remote_directory, filename):
         ssh.close()
 
 
-# Esegui l'acquisizione e trasferimento dell'immagine
-local_path, filename = capture_image_with_timestamp()
-transfer_file_via_sftp(local_path, remote_directory, filename)
+# Esegui l'acquisizione, ridimensionamento e trasferimento dell'immagine
+local_image_path = capture_and_resize_image()
+transfer_file_via_sftp(local_image_path, remote_path)
